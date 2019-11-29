4a6a0dd7e5cb87eca8c6e10efd1e0818dc3abb4d
chockalingam-Bdamx <chockalingam>
2019-11-29 13:59:33 +0530

    backend

--- backend/Nodedetails/devel.js
+++ backend/Nodedetails/devel.js
@@ -1,34 +1,29 @@
 module.exports = {
-	//Demo
-	dbconnection:'mongodb://gseytryhdfgd:ghdftdetyhfdfrrt@192.168.1.75:15897/greygdfhdf',
-	//local	
-	// dbconnection:'mongodb://localhost:27017/Thdbfjsn',
-	port :2095,
+	dbconnection:'mongodb://NsbJSjehwns:PsodnASJenSKSm@172.31.40.194:2332/ReBdaKsmehw',		
+	port : 8443,
 	passPhrase : 'T1Bt0Lx5jA9ML6AJ8523IAv0anRd03Ya',
 	algorithm : 'aes-256-ctr',
 	iv : 'bLMjTTXPjUpWe345',
-	Host : 'https://BDAMX_Revamp.osiztechnologies.in/',	
-	adminhost : 'https://BDAMX_Revampadmin.osiztechnologies.in/#/BdaPLB45MKLXM8957LOO/',	
-	redis_port:6379,
-	redis_host:'192.168.2.91',
-	redis_password:'',	
+	Host : 'https://bdamx.net/',
+	adminhost : 'https://adnsjdnfk.bdamx.net/#/BdaPLB45MKLXM8957LOO/',
+	redis_port:12333,
+	redis_host:'172.31.44.161',
+	redis_password:'HdnKSndkAYsjsd',
+	redisdata:{port:12333,host:'172.31.44.161',password:'HdnKSndkAYsjsd'},
 	cloudinaryData : {
-		cloud_name: 'dz7fyhpef',
-	 	api_key: '745585669876439',
-	 	api_secret: 'P6-pX0F4T2KzVQDpe8-YB30HXQo'
+		cloud_name: 'bdam-network', 
+	 	api_key: '238444197381292', 
+	 	api_secret: '-yXQkz5lyTPLmVw7Pz-olZpaDqU'
 	},
-	redisdata:{port:6379,host:'192.168.2.91'},
 	bitcoin_rpc:{
 		'protocol' :'http',
-		'host' :'192.168.1.62',
-		'user' :'bitcoin',
-		'password' :'Bitcoin',
-		'port' :18332,		
-	},
-	
-	//local (or) demo
-	tradeviewHost:'BDAMX_Revampback.osiztechnologies.in',
-	//tradeviewHost:'192.168.4.12',	
-	api_hit_url_config:"https://api-ropsten.etherscan.io/",
-	api_infura_url:"https://ropsten.infura.io/v3/682d21aed8204bfe8370b50a93aed124"
+		'host' :'172.31.39.213',
+		'user' :'edf43efUJHDiueef',
+		'password' :'UGHU5r65tyfdYdwr3',
+		'port' :54098,		
+	},	
+	//live
+	tradeviewHost:'bdnasudn.bdamx.net',
+	api_hit_url_config:"https://api.etherscan.io/",
+	api_infura_url:"https://infura.io/v3/682d21aed8204bfe8370b50a93aed124"
 }
